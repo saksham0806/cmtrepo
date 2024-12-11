68f67e961abf6d97801d909262b027a32215bad1
merdain0806 <sakshamsingh0806alt@gmail.com>
2024-12-11 22:32:01 +0530

    change no 1

--- bot.py
+++ bot.py
@@ -17,20 +17,19 @@ if not os.stat("log.txt").st_size:
 changelogreader = open("changes.txt",'r+')
 no_to_be_done = int(input("Enter no of commits: "))
 dest_repo = logreader.readlines()[1]
-no_of_changes_done_yet = int(changelogreader.readline())+1
+no_of_changes_done_yet = int(changelogreader.readline())
 
-for i in range(0,no_to_be_done):
-    os.system(f"git remote add origin {dest_repo}")
-    with open("log.txt","a+") as lgwriter:
-        lgwriter.write(f"change no:{no_of_changes_done_yet} done on {datetime.datetime.now()}\n")
-    no_of_changes_done_yet = no_of_changes_done_yet+1
-    if i==no_of_changes_done_yet-1:
-        with open("changes.txt","w") as temp:
-            temp.write(f"{no_of_changes_done_yet-1}")
-        
-    os.system("git add .")
-    os.system(f"git commit -m \"change no {no_of_changes_done_yet}\"")
-    os.system("git push origin master")
+os.system(f"git remote add origin {dest_repo}")
+with open("log.txt","a+") as lgwriter:
+    lgwriter.write(f"change no:{no_of_changes_done_yet} done on {datetime.datetime.now()}\n")
+no_of_changes_done_yet = no_of_changes_done_yet+1
+
+with open("changes.txt","w") as temp:
+    temp.write(f"{no_of_changes_done_yet}")
+    
+os.system("git add .")
+os.system(f"git commit -m \"change no {no_of_changes_done_yet}\"")
+os.system("git push origin master")
 
     
 logreader.close()
